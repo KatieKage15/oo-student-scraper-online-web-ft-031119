accfdefc0d37984095be1cd6f523122592537884
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-02 08:38:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,7 +22,7 @@ class Scraper
     student = {}
     profile_page = Nokogiri::HTML(open(profile_url))
     links = profile_page.css(".social-icon-container").
-    links.each do|link| 
+    links.each do|link|
       if link.include?("linkedin")
         student[:linkedin] = link
       elsif link.include?("github")
