41be60b1cd99f835232c4d90fbc654e6d8165002
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-02 08:13:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,13 +7,14 @@ class Scraper
   def self.scrape_index_page(index_url)
     index_page = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
       index_page.css(".roster").each do |card|
-      card.css(".student-card a").each do |student|
+        card.css(".student-card a").each do |student|
       index_page = student.css(".view").text
 
         binding.pry
     students = []
+        end
+      end
   end
-
   def self.scrape_profile_page(profile_url)
   end
 end
